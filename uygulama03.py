27dee5f7cc79b5c9b6182bf18ab5fe002d71450a
sebahatkecici <36564047+sebahatkecici@users.noreply.github.com>
2018-03-06 21:29:48 +0200

    Add files via upload

---
+++ uygulama03.py
@@ -0,0 +1,13 @@
+#çiçek fiyatlarını gösteren uygulama
+#kullanıcı almak istediği çiçek ismini girdiğnde fiyat bilgisine ulaşır.
+cıceklıstesı = {"gül": "60TL",
+                 "papatya": "30TL",
+                 "yasemin": "25TL",
+                 "orkide": "69TL",
+                 "kaktüs": "10TL",
+                 "karanfil": "20TL",
+                  "lale": "32TL",
+                 "zambak": "45TL",
+                 "kardelen": "88TL"}
+
+print(cıceklıstesı[input("çiçek ismi giriniz:")])
